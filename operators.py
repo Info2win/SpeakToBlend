f6e7dcc6b889fde963fad1e83e77dd7cbeb247b4
unknown <ilker199991@gmail.com>
2023-01-20 00:41:23 +0300

    Fixed some typos

--- operators.py
+++ operators.py
@@ -17,12 +17,12 @@ class _OT_SendCommandToChatGPT(bpy.types.Operator):
         mic = sr.Microphone()
         with mic as source:
             audio = r.listen(source)
-        speach = r.recognize_google(audio)
+        speech = r.recognize_google(audio)
 
         #Talk with chatGPT
         global chatBot
         session_token = bpy.context.scene.SpeakToBlend.session_token
-        command = f'Generate Python code for Blender {bpy.app.version_string} with the following: {speach}. Output only the code without explanations'
+        command = f'Generate Python code for Blender {bpy.app.version_string} with the following: {speech}. Output only the code without explanations'
 
         if not chatBot :
             chatBot = Chatbot({"session_token":session_token})
@@ -35,13 +35,13 @@ class _OT_SendCommandToChatGPT(bpy.types.Operator):
 
         # If code starts with 'python' statement, remove the statemnt
         if code.split('\n')[0] == 'python':
-            code.replace('python\n','')
-
+            code = code.replace('python','')
+        print(code)
         # Create a temporary file to store the code
         with tempfile.NamedTemporaryFile(suffix='.py', delete=False) as temp:
             temp.write(bytes(code, 'utf-8'))
             temp.flush()
-            
+
         # Run the code as a Blender script
         bpy.ops.script.python_file_run(filepath=temp.name)
         
